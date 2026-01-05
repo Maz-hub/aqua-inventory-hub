d7bd9f1cc3968eec3c5b2582c73fb9367eea624e
Maz <77918037+Maz-hub@users.noreply.github.com>
2026-01-05 14:40:52 +0000

    Add 404 Not Found text to NotFound component

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,10 @@
 const NotFound = () => {
   return (
     <>
-      <div>Not Found</div>
+      <div>
+        <h1>404 Not Found</h1>
+        <p>The page you are looking for does not exist!</p>
+      </div>
     </>
   );
 };
